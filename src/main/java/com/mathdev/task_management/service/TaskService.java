bd81bd366f2216fa4af493aa599c59d67f0ce769
matheuslima <mathlb.dev@hotmail.com>
2025-02-12 00:17:25 -0300

    feat: start CRUD implementation, adjust Maven for dependency injection, create TaskDto for data handling, and create TaskConvert for object conversion.

--- src/main/java/com/mathdev/task_management/service/TaskService.java
+++ src/main/java/com/mathdev/task_management/service/TaskService.java
@@ -2,8 +2,10 @@ package com.mathdev.task_management.service;
 
 import com.mathdev.task_management.api.Priority;
 import com.mathdev.task_management.api.Status;
+import com.mathdev.task_management.api.TaskDto;
 import com.mathdev.task_management.db.entity.TaskEntity;
 import com.mathdev.task_management.db.repository.TaskRepository;
+import com.mathdev.task_management.mapper.TaskConvert;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
@@ -13,27 +15,34 @@ import java.time.Instant;
 public class TaskService {
 
     private  final TaskRepository taskRepository;
+    private final TaskConvert taskConvert;
 
-    public TaskService(final TaskRepository taskRepository) {
+    public TaskService(final TaskRepository taskRepository, final TaskConvert taskConvert) {
         this.taskRepository = taskRepository;
+        this.taskConvert = taskConvert;
     }
 
     // Garante que a tarefa de exemplo seja criada ao iniciar a aplicação
     @PostConstruct
     private void initializeSampleTask() {
-        TaskEntity taskEntity = new TaskEntity();
-        taskEntity.setTitle("Apresentação para vaga - Gupy");
-        taskEntity.setDescription("Realizar gravação de apresentação para vaga");
-        taskEntity.setStatus(Status.READY);
-        taskEntity.setPriority(Priority.HIGH);
-        taskEntity.setUpdatedOn(Instant.now());
-        taskEntity.setExpireOn(Instant.now());
-        taskEntity.setCreatedOn(Instant.now());
-        saveTask(taskEntity);
+        TaskDto taskDto = new TaskDto();
+        taskDto.setTitle("Histórico para vaga");
+        taskDto.setDescription("Realizar o envio do histórico para vaga e assistir o video para dar continuação no processo seletivo");
+        taskDto.setStatus(Status.READY);
+        taskDto.setPriority(Priority.HIGH);
+        taskDto.setUpdatedOn(Instant.now());
+        taskDto.setExpireOn(Instant.now());
+        taskDto.setCreatedOn(Instant.now());
+        saveTask(taskDto);
     }
 
-    public void saveTask(final TaskEntity taskEntity){
-        taskRepository.save(taskEntity);
+    public void saveTask(final TaskDto taskDto){
+        try {
+            final TaskEntity taskEntity = taskConvert.convert(taskDto);
+            taskRepository.save(taskEntity);
+        } catch (RuntimeException re){
+            throw re;
+        }
     }
 
 }
